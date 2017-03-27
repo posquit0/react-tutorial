d45e3dffa2452a44bf5eb45c6dbb2bfc90ba58ca
Posquit0.BJ <posquit0.bj@gmail.com>
2017-03-27 21:08:37 +0900

    Complete tutorial 00

---
+++ 00-hello-world/main.js
@@ -0,0 +1,11 @@
+/*
+ * The simplest React component ever.
+ *
+ * It jsut renders an H1 tag into the body of the page. Since
+ * JSX is basically just HTML, you can use any valid tag you want.
+ */
+
+ReactDOM.render(
+  <h1>Hello, World!</h1>,
+  document.getElementById('root')
+);
