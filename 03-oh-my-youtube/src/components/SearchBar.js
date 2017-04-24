c86563337d4e01ab033565f6d36260348c46080b
Posquit0.BJ <posquit0.bj@gmail.com>
2017-04-24 20:06:16 +0900

    Draw SearchBar component

---
+++ 03-oh-my-youtube/src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { Component }  from 'react';
+
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="search-bar">
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
